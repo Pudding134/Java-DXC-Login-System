94351a2edc313e938ab6fc3f89ec4ebd062e3d4a
williechong <williecwy134@gmail.com>
2023-11-20 19:41:47 +0800

    Adding user class to deal with  user information

---
+++ src/main/java/dxc/loginsystem/User.java
@@ -0,0 +1,43 @@
+package dxc.loginsystem;
+
+import java.util.Objects;
+
+public class User {
+    private String userName;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String role) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
